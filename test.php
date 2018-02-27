a927e3647aa38145b46cbee380f1a4edb5759a42
Dylan Baiko <dylanbaiko1@hotmail.com>
2018-02-26 23:01:02 -0800

    commit

--- test.php
+++ test.php
@@ -11,37 +11,44 @@
 	$servername = "localhost";
 	$username = "cswebhosting";
 	$password = "a9zEkajA";
-		    
-	$conn = new mysqli($servername, $username, $password, $username);
+
 	
-	
-	if($conn->connect_error){
-	    echo("Connection Failed");
-	    die("Connection failed: " . $conn->connect_error);
+	if(isset($_POST["submit"])){
+	    $check = getimagesize($_FILES["file"]["tmp_name"]);
+	    if($check !== false){
+	        $image = $_FILES["file"]["tmp_name"];
+	        $imgContent = file_get_contents($image);
+	        
+	        
+	        $conn =  mysqli_connect($servername, $username, $password, $username);
+	        
+	       
+	        #inserting
+	        $sql = $conn->prepare("INSERT INTO Test (userName,file) VALUES (?,?);");
+	        
+	        $null = NULL;
+	        $t = "fuck";
+	        
+	        $sql->bind_param("sb", $t,$null);
+	        
+	        $sql->send_long_data(1, $imgContent);
+	        
+	        $sql->execute();
+	        
+	        #retreiving
+	        $stmt = "SELECT file FROM Test";
+	        
+	        
+	        $res = $conn->query($stmt);
+	        while($result=mysqli_fetch_array($res)){;
+	           echo '<img src="data:image/png;base64,'.base64_encode($result['file']).'"/>';
+	        }
+
+	        
+	        
+	    }
 	}
 	
-	$sql = "INSERT INTO Test VALUES ('Bob', 'test321')";
-	
-	
-	if ($conn->query($sql) === TRUE) {
-	    echo "New record created successfully";
-	} else {
-	    echo "Error: " . $sql . "<br>" . $conn->error;
-	}
-	
-	$conn->close();
-	
-	
-	#if(isset($_POST["submit"])){
-	#    echo("Welcom: ".$_POST["userName"] . "<br> \n");
-	#    echo("Password: " . $_POST["password"] . "<br>");
-	 #   
-	  #  $conn = new mysqli_connect($servername, $username, $password);
-	    
-	#}
-	#else{
-	#    echo("no data");
-	#}
 	
 	?>
 	
