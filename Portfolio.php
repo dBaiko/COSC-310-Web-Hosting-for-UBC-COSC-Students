56963e09d908f79da5b9b4bb1a7b59271d242a38
Darrien Broomfield <darrienbroomfield@hotmail.com>
2018-03-21 16:24:33 -0700

    fixed portfolio styling

--- Portfolio.php
+++ Portfolio.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 session_start();
 if(isset($_SESSION['user'])){
     $user = $_SESSION['user'];
@@ -7,6 +9,116 @@ if(isset($_SESSION['user'])){
 
 <?php 
 if(isset($_SESSION['user'])){
+
+  class listContent{
+      
+      private $db_host = 'localhost';
+      private $db_name = 'cswebhosting';
+      private $db_user = 'cswebhosting';
+      private $db_pass = 'a9zEkajA';
+      private $conn = null;
+      private  $db = null;
+      
+      
+      public function __construct() {
+          $this->conn = mysqli_connect($this->db_host, $this->db_user, $this->db_pass);
+          if(!$this->conn){
+              die('Could not connect: ' .mysqli_error());
+          }
+          else{
+              $this->db = mysqli_select_db($this->conn, $this->db_name);
+              if(!$this->db){
+                  die('Could not connect: ' .mysqli_error());
+              }
+          }
+      }
+      
+      public function __destruct(){
+          $this->conn->close();
+          $this->conn = null;
+      }
+      
+      //This function takes in username, firstname, lastname, studentnumber, email, school, major
+    public function displayContent($param1, $param2, $param3, $param4, $param5, $param6, $param7){
+          echo '<h2 id = "name">'.$param1.'</h2>
+				<table  id ="info">
+				<thead>
+					<tr>
+						<th>Name</th>
+                        <th>Student Number</th>
+						<th>Email</th>
+						<th>School</th>
+                         <th>Major</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>'.$param2.' '.$param3.'</td>
+						<td>'.$param4.'</td>
+						<td>'.$param5.'</td>
+                        <td>'.$param6.'</td>
+                        <td>'.$param7.'</td>
+					</tr>
+				</tbody>
+			</table>';
+ 
+      }
+      
+      public function query_userInfo($param1){
+          include 'php/all_projects.php';
+          if ($conn->connect_error) {
+              die("Connection failed:" . $conn->connect_error);
+          }
+             $sql = "SELECT *
+                     FROM User JOIN Student ON User.userName = Student.userName
+                     WHERE User.userName = \"$param1\"
+                     GROUP BY User.UserName;";
+          
+          $result = mysqli_query($conn, $sql);
+          return $result;
+      }
+      
+      public function displayAllProjects($userName){
+          if($this->conn != null){
+              $stm = "SELECT p.projectId, p.projectTitle, pub.userName, p.date, p.projDesc, p.logoImage FROM Project AS p, Published AS pub WHERE p.projectId = pub.projectId AND pub.userName = ? GROUP BY pub.projectId ORDER BY p.date DESC";
+              if($sql = $this->conn->prepare($stm)){
+                  $sql->bind_param("s",$userName);
+                  if($sql->execute()){
+                      $sql->bind_result($id,$title,$u,$date,$desc,$logo);
+                      while($sql->fetch()){
+                          $this->displayPContent($title,$u,$date,$desc,$logo,$id);
+                      }
+                      return true;
+                  }else {
+                      $error = $this->conn->errno . ' ' . $this->conn->error;
+                      echo $error;
+                      return false;
+                  }
+              }else {
+                  $error = $this->conn->errno . ' ' . $this->conn->error;
+                  echo $error;
+                  return false;
+              }
+          }
+          else{
+              return false;
+          }
+      }
+      
+      public function displayPContent($param1, $param2, $param3, $param4, $param5,$param6)
+      {
+          echo "<a href=\"viewProject.php?projectId=$param6\" style=\"\"><table class=\"project\" id=\"website\">" . "<caption>" . $param1 . "</caption>" . "<thead>" . "<tr>" . "<th class=\"textleft\"> By: " . $param2 . "</th>" . "<th class=\"textright\">" . $param3 . "</th>" . "</tr>" . "</thead>" . "<tbody>" . "<tr>" . "<td colspan=\"2\"><img src=\"data:image/png;base64,".base64_encode($param5)."\"name=\"web\"class=\"images\" alt=\"logo here\" />" . "<p class=\"desc\">" . $param4 . "</p></td>" . "</tr>" . "</tbody>" . "<tfoot>
+    <tr>
+    <td colspan=\"2\">
+    <p id=\"copyright\">Copyright &copy; " . $param1 . "</p>
+    </td>
+    </tr>
+    </tfoot>
+    </table></a>";
+      }
+      
+       
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -45,71 +157,32 @@ if(isset($_SESSION['user'])){
 
 <div id = "main">
 	<div id ="profileInfo">
-		<img src = "Images/SunsLogo.png" alt = "ProfileImage" id = "profile"/>
-		<div>
-			<h2 id = "name"> DBroomfield</h2>
-				<table  id ="info">
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>User Type</th>
-						<th>School/Business</th>
-						<th>	Degree</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>Darrien Broomfield</td>
-						<td>Student</td>
-						<td> UBC Okanagan</td>
-						<td> Math / COSC </td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+<?php 
+if(isset($_SESSION['user'])){
+    $user = $_SESSION['user'];
+}
+    $contentGet = new listContent();
+
+        $result = $contentGet->query_userInfo($user);
+        $resultCheck = mysqli_num_rows($result);
+        
+        if($resultCheck > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $contentGet->displayContent($row['userName'], $row['firstName'], $row['lastName'], $row['studentNum'], $row['email'], $row['school'], $row['major']);
+            }
+        }else echo "Information not available at the moment!";
+    ?>
 	</div>
 	<div id = "background">
 	<h2>Projects:</h2>
 	<div id = "projects">
-		<table id = "project">
-						<caption >COSC 310 Project</caption>
-						<thead>
-							<tr>
-								<th>By: DBroomfield</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>
-									<img src = "Images/BuildingWebsite.jpg" name = "web" id = "web"/>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pellentesque 
-									iaculis arcu, non consequat lacus maximus et. Quisque a nulla imperdiet, 
-									blandit mauris eget, fermentum quam. Maecenas dapibus vitae arcu ut varius. 
-									Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vel 
-									aliquam massa. Aliquam id metus dui. In vitae lacus porta, ullamcorper massa ultricies, 
-									rhoncus leo. Donec mattis tellus imperdiet ex finibus, eget porttitor elit pharetra. 
-									In at quam dapibus, auctor odio a, feugiat metus. Proin pretium fringilla nisl convallis hendrerit. 
-									Fusce vehicula molestie metus et elementum. Nullam egestas egestas sapien sed mattis.
-									Vestibulum eget dictum augue. Aenean fringilla, ipsum non tempor cursus, urna nunc semper ipsum, 
-									et tempus sapien magna eget lacus. Curabitur rutrum ex felis, et dapibus purus faucibus ac. 
-									Morbi pharetra risus ac felis auctor, et pretium magna auctor. Quisque a sem lobortis, 
-									vulputate turpis eget, eleifend ante. Suspendisse fringilla sapien quis nibh euismod dictum. 
-									Sed eleifend quis dui nec pellentesque. Vestibulum placerat enim consequat eros consectetur 
-									ultricies. Nulla eu dui ut enim porta efficitur. Mauris nibh nisl, vulputate non iaculis et, 
-									lobortis vel odio. Aliquam erat volutpat. Pellentesque dolor nisl, egestas sit amet nisl at, 
-									molestie volutpat metus. Sed posuere nunc neque, in luctus magna pretium at.
-									</p>
-								</td>
-							</tr>
-						</tbody>
-						<tfoot>
-							<tr>
-								<td>
-									<p id = "copyright"> Copyright &copy; 2018 COSC 310 Project </p>
-								</td>
-							</tr>
-						</tfoot>	
-					</table>
+	
+<!-- 	display user project goes here -->
+	<?php 
+	   $contentGet->displayAllProjects($user);
+	   $contentGet = null;
+	?>
+	
 	</div>
 	</div>
 </div>
